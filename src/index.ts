485d1675b2e52150d8aa9953927a7f0a0a884742
Trevor Burnham <trevorburnham@gmail.com>
2025-04-24 08:13:54 -0400

    Fix for "rule not found" when named export is expected

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import debarrel from "./rules/debarrel.js";
 
+// Export rules as a named export
+export const rules = {
+  debarrel,
+};
+
+// Export as a default export
 const plugin = {
   rules: {
     debarrel,
   },
-};
-
+}
 export default plugin;
